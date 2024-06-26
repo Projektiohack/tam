63cb3b75d16f2522babd2cd23618b96446416555
Projektiohack <93693938+Projektiohack@users.noreply.github.com>
2024-06-26 02:45:05 -0400

    install ngrok v1
    hai sobat termux

---
+++ installsetuo.sh
@@ -0,0 +1,20 @@
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+yellow='\033[clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+yellow='\033[33;1m'
+sleep 1
+clear
+echo
+tiolet -f big -F gay install
+echo $cyan
+apt update && upgrade
+apt install wget
+apt install toilet
+
